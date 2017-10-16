98d85e944d8ea21f6d9d066c03a3a940da43526d
freedomcondor <zhuweixu_harry@126.com>
2017-10-16 16:24:09 +0200

    first success

--- hungarian.lua
+++ hungarian.lua
@@ -42,6 +42,37 @@ local function getNM_Mat(Mat)
 	return i,maxM
 end
 
+-- queue
+----------------------------------------------------------------
+local Queue = {}
+Queue.__index = Queue
+function Queue:create()
+	local instance = {first = 1; last = 0}			
+	setmetatable(instance, self)
+	return instance
+		--starts from 1	,  read from first,  add to next of last
+		--		 first        		last  
+		--       x   	x  x  x  x 	x 		(next)
+end
+
+function Queue:add(x)
+	self.last = self.last + 1
+	self[self.last] = x
+end
+
+function Queue:read()
+	if self.first > self.last then return nil end
+	local value = self[self.first]
+	self[self.first] = nil
+	self.first = self.first + 1
+	return value
+end
+
+function Queue:isEmpty()
+	if self.first > self.last 	then return true 
+								else return false end
+end
+
 ----------------------------------------------------------------
 -- Hungarian starts
 	-- for the algorithm, please refer: https://www.topcoder.com/community/data-science/data-science-tutorials/assignment-problem-and-hungarian-algorithm/
@@ -96,12 +127,25 @@ function Hungarian:create(configuration)
 	end
 	instance.N = n
 
+	---------------- min to max problem ----------------
+	for i = 1,n do
+		for j = 1,n do
+			instance.costMat[i][j] = -instance.costMat[i][j] 
+		end
+	end
+	----------------------------------------------------
+
 	-- Set labels and maxMatch
-	--local i,j -- maybe in lua this is not necessary, the i in for is local to for
+	--local i,j -- in lua this is not necessary, the i in for is local to for
 	instance.maxMatch = 0
+	instance.match_of_X = {}
+	instance.match_of_Y = {}
+
+		--init lx,ly
 	instance.lx = {}; instance.ly = {}
-	for i = 1,n do instance.lx[i] = 0 end
+	for i = 1,n do instance.lx[i] = instance.costMat[1][1] - 99999999999 end  -- set to -INF
 	for i = 1,n do instance.ly[i] = 0 end
+	--lx is the max of his cost edge		-- for min problem
 	for i = 1,n do 
 		for j = 1,n do
 			if instance.lx[i] < instance.costMat[i][j] then
@@ -113,6 +157,55 @@ function Hungarian:create(configuration)
 
 	return instance
 end
+
+------------------------------------------------------------------------------------------
+function Hungarian:update_labels()
+	local N = self.N
+	local slack = self.slack
+	local slackx = self.slackx
+
+	local delta = slack[1] + 99999999  -- for max set as INF
+
+	-- find the min delta among slack
+	for y = 1,N do
+		if self.T[y] ~= true and slack[y] < delta then	
+			delta = slack[y]
+		end
+	end
+
+										-------------------------
+										--print("delta = ",delta)
+
+	-- update delta change
+	for x = 1,N do
+		if self.S[x] == true then
+			self.lx[x] = self.lx[x] - delta		--max
+		end
+	end
+	for y = 1,N do
+		if self.T[y] == true then
+			self.ly[x] = self.ly[x] + delta		--max
+		end
+	end
+
+	-- update slack
+	for y = 1,N do
+		if self.T[y] ~= true then slack[y] = slack[y] - delta  end --max
+	end
+end
+function Hungarian:add_to_tree(x,its_parent)
+	self.S[x] = true
+	self.parent_table[x] = its_parent
+	
+	-- update slack of this new x in S
+	for y = 1,self.N do
+		if (self.lx[x] + self.ly[y] - self.costMat[x][y] < self.slack[y]) then			--max
+			self.slack[y] = self.lx[x] + self.ly[y] - self.costMat[x][y]		-- max
+			self.slackx[y] = x
+		end
+	end
+end
+---The Augment-------------------------------------------------------------------------------
 
 function Hungarian:aug()
 	--[[
@@ -134,19 +227,23 @@ function Hungarian:aug()
 		-- and a queue ?? 
 			--the use of the queue is xxxxx
 		-- a slack for find the min delta quickly
-	
-	------------------------Init everything--------
+
+	----Find a single x and Init everything--------------
+
 	-- Init everything
-	local S = {}
-	local T = {}
-	local parent_table = {}
-	--local queue = {}
-	local root
+	self.S = {}
+	self.T = {}
+	self.parent_table = {}
+	local S = self.S
+	local T = self.T
+	local parent_table = self.parent_table
+	local queue = Queue:create()
+	local root 
 
 	-- find a single x
 	for x = 1,N do
 		if self.match_of_X[x] == nil then
-			--queue.add(x)
+			queue:add(x)
 			root = x
 			parent_table[x] = -2
 			S[x] = true
@@ -158,13 +255,170 @@ function Hungarian:aug()
 	-- init slack
 		-- slack is used for store the mini delta for each y
 		-- slackx is used for store to which x this mini delta is achieved
-	slack = {} slackx = {}
-	for y = 1,N do slack[y] = 
-		self.lx[root] + self.ly[root] - self.cost[root][y] end
+	self.slack = {} self.slackx = {}
+	local slack = self.slack 
+	local slackx = self.slackx
+
+	for y = 1,N do 
+		slack[y] = self.lx[root] + self.ly[root] - self.costMat[root][y] end		-- max
 	for y = 1,N do slackx[y] = root end
 
-	-----------------------------------------------
-end
+	-----Start to find------------------------------------
+
+										--------debug---------------
+										--print("start to find,root is",root)
+										--io.read()
+
+	local edgex = nil		-- used for record the edge if a good path is found
+	local edgey = nil
+	local flag = 0
+	while true do
+
+										--------debug---------------
+										--print("queue is Empty",queue:isEmpty())
+										--io.read()
+
+		-- search every x in queue
+		while queue:isEmpty() == false do
+			x = queue:read()
+										--------debug---------------
+										--print("focal x",x)
+										--io.read()
+			-- for this new x, find all its edges
+			flag = 0	--flag = 1 mean found a good path
+			for y = 1,N do
+										--------debug---------------
+										--print("focal y",y)
+										--io.read()
+				-- search y, find a edge of equality between this new x and a new y (not in T)
+				if self.costMat[x][y] == self.lx[x] + self.ly[y] and T[y] ~= true then
+										--------debug---------------
+										--print("focal y is equal")
+										--io.read()
+					-- check is this y assigned? if not means a good path found
+					if self.match_of_Y[y] == nil then 
+										--------debug---------------
+										--print("a path found")
+										--io.read()
+						edgex = x; edgey = y
+						flag = 1; break 
+					end  
+						-- a good path found, jump out of for y(search for y) 
+
+					-- y is assigned, add its x to tree
+					T[y] = true
+					queue:add(self.match_of_Y[y])
+					--S[x] = true   -- this is done in add_to_tree
+					self:add_to_tree(self.match_of_Y[y],x)
+				end
+			end	-- for y
+
+			if flag == 1 then break end
+				-- a good path found jump out of while queue)
+				-- else next x in queue
+		end	-- end of queue searching (while queue:isEmpty)
+
+										--------debug---------------
+										--print("test")
+		if flag == 1 then break end
+			-- a good path found jump out of this x searching
+			-- else means we have searched every x in S
+			--    			tree is built
+			--				need to change label next
+		
+		self:update_labels()
+	
+										------debug------------------
+										--[[
+										---self:add_to_tree(2,1)
+										--self:add_to_tree(3,1)
+										print("parent_table:")	
+										for x = 1,N do
+											print(parent_table[x])	
+										end
+
+										print("slack_table:")	
+										for x = 1,N do
+											print(slack[x])	
+										end
+
+										print("lx_table:")	
+										for x = 1,N do
+											print(self.lx[x])	
+										end
+										print("ly_table:")	
+										for y = 1,N do
+											print(self.ly[y])	
+										end
+										--]]
+										----------------------------
+		queue = nil; queue = Queue:create()
+		flag = 0
+		for y = 1,N do
+			if T[y] ~= true and slack[y] == 0 then
+				-- means a new equal edge is found, a new Y
+				if self.match_of_Y[y] == nil then
+					-- this Y is single
+										-----------------------------
+										--print("new edge found after changing label")
+										--io.read()
+										-----------------------------
+
+					x = slackx[y]
+					flag = 1
+					edgex = x; edgey = y
+					break
+				else
+					-- this Y is not single
+					T[y] = true
+					if S[self.match_of_Y[y]] == nil then
+						queue:add(self.match_of_Y[y])
+						self:add_to_tree(self.match_of_Y[y],slackx[y])
+					end
+				end
+			end
+		end
+
+		if flag == 1 then break end
+	end	-- end of x searching (while true)
+
+	if flag == 1 then  -- a good path
+		self.maxMatch = self.maxMatch + 1
+
+										----------------------------
+										--print("edgex edgey = ",edgex,edgey)
+										--io.read()
+
+		------ change the path -------
+		local px = edgex
+		local py = edgey
+		local temp
+		while px ~= -2 do
+			temp = self.match_of_X[px]
+			self.match_of_Y[py] = px
+			self.match_of_X[px] = py
+			py = temp
+			px = parent_table[px]
+		end
+		-------------------------------
+										----debug-------------------------
+										--[[
+										print("match table X")
+										for x = 1,N do
+											print(self.match_of_X[x])	
+										end
+										print("match table Y")
+										for y = 1,N do
+											print(self.match_of_Y[y])	
+										end
+										--]]
+										----------------------------------
+		self:aug()
+	end
+
+	-- if it comes here, that means all the possibility is tried and no other edges can be add,
+	-- then it is the end, match table is what we got
+end	--end of function aug
 
 return Hungarian
 
--- main.lua
+++ main.lua
@@ -1,15 +1,27 @@
-Hungarian = require("Hungarian")
+Hungarian = require("hungarian")
 
+--[[
 costMat = {	{1,2,3},
 			{4,5,6},
 			{4,5,6},
+			}
+--]]
+costMat = {	{1,4,5},
+			{5,7,6},
+			{5,8,8},
 			}
 
 hun = Hungarian:create{costMat = costMat
 					  }
 
-print("hun.costMat=",hun.costMat[1][1])
-print("hun.N=",hun.N)
-for i,v in ipairs(hun.lx) do
-	print("hun.lx[",i,"]=",v)
+
+hun:aug()
+
+print("match table X")
+for x = 1,hun.N do
+	print(hun.match_of_X[x])
+end
+print("match table Y")
+for y = 1,hun.N do
+	print(hun.match_of_Y[y])
 end
